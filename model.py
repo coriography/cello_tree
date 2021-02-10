656d7348cad26f822b470763b0aff4f2d69898ec
Cori Lint <corilint@gmail.com>
2021-02-10 14:37:28 -0600

    added relationships between all db models

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ class Cellist(db.Model):
     owner_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     creator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     editor_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    # link = db.Column(db.Integer, db.ForeignKey('links.link_id'))
+    link = db.Column(db.Integer, db.ForeignKey('links.link_id'))
     fname = db.Column(db.String(50), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
     location = db.Column(db.String(50))
@@ -23,6 +23,7 @@ class Cellist(db.Model):
     img_url = db.Column(db.Text)
     music_url = db.Column(db.Text)
 
+    creator = db.relationship('User', backref='cellist_profiles')
     # links: a list of Link objects associated with Cellist.
 
     def __repr__(self):
@@ -45,15 +46,15 @@ class Link(db.Model):
     end_year = db.Column(db.DateTime)
     location = db.Column(db.String(50))
 
-    # teacher = db.relationship('Cellist', foreign_keys='teacher_id', backref='teacher_links')
-    # student = db.relationship('Cellist', foreign_keys='student_id', backref='student_links')
+    teacher = db.relationship('Cellist', foreign_keys=[teacher_id], backref='teacher_links')
+    student = db.relationship('Cellist', foreign_keys=[student_id], backref='student_links')
 
     def __repr__(self):
         """Display info about Link."""
 
-        return f'<Link link_id={self.link_id} start_year={self.start_year}>'
+        return f'<Link link_id={self.link_id} teacher_student_link={self.teacher} / {self.student}>'
 
-    # link = Link(start_year='2014', end_year='2018', location='Cleveland')
+    # link = Link(teacher_id='1', student_id='1')
 
 
 class Post(db.Model):
@@ -62,11 +63,20 @@ class Post(db.Model):
     __tablename__ = "posts"
 
     post_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    cellist_id = db.Column(db.Integer, db.ForeignKey('cellists.cellist_id'), nullable=False)
+    post_date = db.Column(db.DateTime, nullable=False)
+
+    # upvotes: a list of Upvote objects associated with Post. 
+    cellist = db.relationship('Cellist', backref='posts')
+    user = db.relationship('User', backref='posts')
 
     def __repr__(self):
         """Display info about Post."""
 
-        return f'<Post post_id={self.post_id}>'
+        return f'<Post post_id={self.post_id} cellist_id={self.cellist_id}>'
+
+        # post = Post()
 
 
 class Upvote(db.Model):
@@ -75,7 +85,12 @@ class Upvote(db.Model):
     __tablename__ = "upvotes"
 
     upvote_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'))
     count = db.Column(db.Integer, nullable=False)
+
+    post = db.relationship('Post', backref='upvotes')
+    user = db.relationship('User', backref='upvotes')
 
     def __repr__(self):
         """Display info about Upvote."""
@@ -92,6 +107,11 @@ class User(db.Model):
     username = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.String(50), nullable=False) # or use Role table???
+
+    # cellist_profiles: a list of Cellist objects associated with User.
+    # posts: a list of Post objects associated with User.
+    # upvotes: a list of Upvote objects associated with User.
 
     def __repr__(self):
         """Display info about User."""
@@ -100,22 +120,25 @@ class User(db.Model):
 
         # test_user = User(username='tester', email='test@test.com', password='123')
 
-class Role(db.Model):
-    """Data model for a user/admin role."""
+# class Role(db.Model):
+#     """Data model for a user/admin role."""
 
-    __tablename__ = "roles"
+#     __tablename__ = "roles"
 
-    role_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    admin = db.Column(db.Boolean)
-    moderator = db.Column(db.Boolean)
-    user = db.Column(db.Boolean)
+#     role_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+#     admin = db.Column(db.Boolean)
+#     moderator = db.Column(db.Boolean)
+#     r_user = db.Column(db.Boolean)
+#       # or: role = db.Column(db.String(50)) #admin, moderator, or user
+#       # user = db.relationship('User', backref='role')
 
-    def __repr__(self):
-        """Display info about Role."""
+#     def __repr__(self):
+#         """Display info about Role."""
 
-        return f'<Role role_id={self.role_id}, user={self.user}>'
+#         return f'<Role role_id={self.role_id}, user_id={self.user_id}>'
 
-        # tr = Role(admin='False', moderator='False', user='True')
+#         # tr = Role(admin='False', moderator='False', user='True')
 
 
 def connect_to_db(flask_app, db_uri='postgresql:///test', echo=True):
