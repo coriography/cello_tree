6a5d21247393dfa7d3d4532ae7fca277553a01b6
Cori Lint <corilint@gmail.com>
2021-02-10 17:28:15 -0600

    added server route and template for home page

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from model import connect_to_db
 
@@ -7,3 +7,12 @@ from jinja2 import StrictUndefined
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def show_home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
