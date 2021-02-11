c3e149b73fa4b67e775ee17e63fdd94e8c5ae483
Cori Lint <corilint@gmail.com>
2021-02-11 15:34:38 -0600

    created ajax req stub and server route stub for add_cellist

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 from model import connect_to_db
 
+from crud import create_cellist
+
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
@@ -10,14 +12,21 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def show_home():
+
     return render_template('home.html')
 
 @app.route('/add_cellist')
 def add_cellist():
-    # create_cellist(
+
     return render_template('add_cellist.html')
 
+@app.route('/data/<int:cellist_id')
+def add_cellist():
+    """Add a cellist to the database."""
 
+    # create_cellist(fname, lname, location, cello_details, bio, img_url, music_url)
+    
+    return "this is the return from server"
 
 
 if __name__ == '__main__':
---
+++ static/ajax.js
@@ -0,0 +1,9 @@
+$('#add_cellist').on('submit', (evt) => {
+    evt.preventDefault();
+
+    //get form input
+
+    //send data to server.py
+
+        //display response
+});
