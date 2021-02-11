0f13e0d82039632d252e9325b407c810aee130bb
Cori Lint <corilint@gmail.com>
2021-02-10 21:18:37 -0600

    created seed data code loop for cellists

--- seed.py
+++ seed.py
@@ -1,12 +1,23 @@
 import os
-
 import model
+import server
+import crud
 
-# os.system('dropdb tree')
-# os.system('createdb tree')
+from test_data import data
 
-# model.connect_to_db(server.app)
-# model.db.create_all()
+os.system('dropdb tree')
+os.system('createdb tree')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+
+for cellist in data:
+    crud.create_cellist(cellist['fname'], cellist['lname'], cellist['location'], cellist['cello_details'], cellist['bio'], cellist['img_url'], cellist['music_url'])
+
+
+if __name__ == '__main__':
+    print("we're in seed")
 
 
 
