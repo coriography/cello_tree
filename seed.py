59b030fe60bbaabb5504b80bde8b8dc71d16cf32
Cori Lint <corilint@gmail.com>
2021-02-12 18:44:20 -0600

    added seed user data and homepage login form and about blocks

--- seed.py
+++ seed.py
@@ -3,7 +3,7 @@ import model
 import server
 import crud
 
-from test_data import data
+from cellist_data import data
 
 os.system('dropdb tree')
 os.system('createdb tree')
@@ -11,9 +11,19 @@ os.system('createdb tree')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-
+# seed cellist data from cellist_data.py
 for cellist in data:
     crud.create_cellist(cellist['fname'], cellist['lname'], cellist['cello_details'], cellist['bio'], cellist['img_url'], cellist['music_url'])
+
+
+# seed user data
+for i in range(10):
+
+    username = f"username{i}"
+    email = f"email{i}@email.com"
+    password = f"password{i}"
+
+    crud.create_user(username, email, password)
 
 
 if __name__ == '__main__':
