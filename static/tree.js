a0fe0e8352cb85c13ebc0fbf12f0ea98e1932cc1
Cori Lint <corilint@gmail.com>
2021-02-21 13:43:26 -0600

    tree setup: added sample data, js file, header block, and graph container

---
+++ static/tree.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var data = {
+    "name": "A1",
+    "children": [
+      {
+        "name": "B1",
+        "children": [
+          {
+            "name": "C1",
+            "value": 100
+          },
+          {
+            "name": "C2",
+            "value": 300
+          },
+          {
+            "name": "C3",
+            "value": 200
+          }
+        ]
+      },
+      {
+        "name": "B2",
+        "value": 200
+      }
+    ]
+  }
+
+const root = d3.hierarchy(data)
