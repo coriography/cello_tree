5f690f713fce86dcbe56cb06a1dd3e37d5393b22
Cori Lint <corilint@gmail.com>
2021-02-21 19:16:17 -0600

    corrected teacher references

--- cellist_data.py
+++ cellist_data.py
@@ -21,7 +21,7 @@ data = [
     'bio': 'Since 2001, Martha has been a member of the cello section of The Cleveland Orchestra.  As a performer she has appeared extensively in solo and chamber music performances across the globe and has been broadcast on NPR and CBC radio nationally. A passionate and dedicated teacher, Martha has taught at The Cleveland Institute of Music for more than a decade and served as Chair of the Preparatory String department for 5 years. She has given classes and seminars across the U.S. and Canada and regularly serves on competition juries in the U.S. and Canada.  Martha’s students have graduated from nearly every major conservatory and university music school in the U.S. and are frequent winners of numerous competitions. \n \n A native of Calgary, Alberta, Martha is a graduate of Rice University’s Shepherd School of Music and The Cleveland Institute of Music and has studied with such renowned pedagogues as John Kadz, Harvey Shapiro, Paul Katz, Desmond Hoebig, Aldo Parisot, and Stephen Geber.  Martha is an active teaching artist and leader in The Cleveland Orchestra’s Education Department where she works on everything from concert scripts to strategic planning. Martha lives in the Cleveland area with her husband, daughter, and dog and plays on a cello made by Eugene Holtier of North Ridgeville, Ohio.',
     'img_url': 'https://theplannedpractice.com/wp-content/uploads/2019/09/IMG_5532-1024x831.jpeg',
     'music_url': 'https://theplannedpractice.com/',
-    'teacher_ids': [3]
+    'teacher_ids': [5]
     },
     {'fname': 'Melissa',
     'lname': 'Kraut',
@@ -29,7 +29,7 @@ data = [
     'bio': 'Co-head of the cello department at the Cleveland Institute of Music, Dr. Melissa Kraut is recognized as one of the leading pedagogues of her generation. Having developed and trained some of the outstanding young musicians of today, Dr. Kraut has demonstrated a unique ability to teach all ages and stages of dedicated students, helping them reach their highest potential both at and away from the cello. \n With degrees from the Cleveland Institute of Music, the University of Iowa and Northwestern University, Dr. Kraut has had the opportunity to study with the great pedagogues Alan Harris and Hans-Jorgen Jensen as well as summer study/master classes with cellists such as Aldo Parisot, Frank Miller, Yo-Yo Ma, Tsuyoshi Tsutsumi and David Soyer. As a student, she participated in the Aspen Music Festival, Banff Center for the Arts and the Schleswig-Holstein Music Festival in Europe. \n An active performer, Dr. Kraut has led a diverse career on stage, with solo and chamber performances throughout the United States and Europe. She has held leadership positions in several orchestras, and has played under the baton of conductors such as Sir Georg Solti, Valery Gergiev and Semyon Bychkov. \n Dr. Kraut currently enjoys performing chamber music with her friends and colleagues throughout the world. \n Dr. Kraut enjoys reaching students from all over the world through master classes and workshops. Her status as a Suzuki Teacher Trainer, enables her to pass on her love of pedagogy to the next generation of teachers. In addition, she is passionate about public speaking and the ability to reach audiences of a larger scope, about topics broader than cello. \n In the summer of 2014, Dr. Kraut and famed cellist Zuill Bailey launched the inaugural summer of the Sitka Cello Seminar in Sitka, Alaska, bringing 10 elite cellists from all over the world to study under their guidance. In prior summers, Dr. Kraut was on the faculty of several summer festivals including eight summers at the Meadowmount School of Music and eight summers at Interlochen Arts Camp, where she was also the Head of Strings. Other festivals include the Lev Aronson Legacy Week in Dallas, TX, as well as Heifetz International Music Institute in Staunton, Virginia. \n Dr. Kraut’s students have achieved great success, with top prizes in National and International competitions. Students of Dr. Kraut have won the Gold Medal and Audience Award at the Gaspar Cassado Competition in Hachioji, Japan, Grand Prize in the Music Teachers National Association Competition, First Prize in the American String Teacher’s Association, Grand Prize in the Walgreen’s Competition, Grand Prize in the Fischoff Competition, as well as prizes in many local and regional competitions.',
     'img_url': 'https://static.wixstatic.com/media/dba320_6907ac2ba38d4b419a5c2d3a1e71bb27~mv2.png/v1/fill/w_1576,h_896,al_c,q_90,usm_0.66_1.00_0.01/dba320_6907ac2ba38d4b419a5c2d3a1e71bb27~mv2.webp',
     'music_url': 'https://www.melissakraut.com',
-    'teacher_ids': [3]
+    'teacher_ids': [5]
     },
     {'fname': 'Hans',
     'lname': 'Jørgen Jensen',
@@ -37,7 +37,7 @@ data = [
     'bio': 'teacher Leonard Rose',
     'img_url': 'https://music.northwestern.edu/sites/default/files/2017-11/HansJorgenJensen.jpg',
     'music_url': '',
-    'teacher_ids': [3]
+    'teacher_ids': []
     },
     {'fname': 'János',
     'lname': 'Starker',
@@ -45,7 +45,7 @@ data = [
     'bio': '',
     'img_url': '',
     'music_url': '',
-    'teacher_ids': [3]
+    'teacher_ids': []
     },
     {'fname': 'Brant',
     'lname': 'Taylor',
@@ -53,7 +53,7 @@ data = [
     'bio': '',
     'img_url': '',
     'music_url': '',
-    'teacher_ids': [3]
+    'teacher_ids': [6]
     },
     {'fname': 'Frank',
     'lname': 'Miller',
@@ -61,7 +61,7 @@ data = [
     'bio': '',
     'img_url': '',
     'music_url': '',
-    'teacher_ids': [3]
+    'teacher_ids': []
     },
 ]
 
