c27bd6c97fa534ce15f9381c5e36a1932767b720
Cori Lint <corilint@gmail.com>
2021-06-01 19:29:40 -0500

    created test for adding cellist to db

--- test_db.py
+++ test_db.py
@@ -3,7 +3,7 @@ from crud import *
 
 def test_all():
     test_user()
-    # test_cellist()
+    test_cellist()
     # test_link()
     # test_post()
     # test_upvote()
@@ -13,15 +13,9 @@ def test_user():
     create_user('Guppy', 'guppy@thecat.com', 'badpw')
 
 
-# def test_cellist():
-#     """Creates test Cellist in test database"""
-#     upload_image(
-#         'https://s.keepmeme.com/files/en_posts/20200822/cc83fa3c7f8f8d04b3cdb12d65d57101confused-cat-with-a-lot-of-question-marks.jpg',
-#         'this cat is CONFUSE',
-#         1,
-#         False,
-#         None
-#         )
+def test_cellist():
+    """Creates test Cellist in test database"""
+    create_cellist('Guppy', 'The Cat', 'Guppy plays a teeny tiny cello made in 2018 in Tulsa, OK', 'Guppy started playing cello at the young age of 6 weeks. She became a virtuoso and made her debut at Carnegie Hall in 2019.', img_url="", music_url="")
 
 
 # def test_link():
